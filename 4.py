81a85bc2b9023bcd5f5e99af577dff246594836e
phantronganh <62613290+phantronganh@users.noreply.github.com>
2020-05-25 11:04:00 +0700

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,12 @@
+from tkinter import *
+window = Tk()
+window.title(" Window form ")
+window.geometry('350x200')
+lbl = Label(window, text="Hello")
+lbl.grid(column=0, row=0)
+def clicked():
+    lbl.configure(text="Button was clicked !!")
+    
+btn=Button(window, text="Click Me", command=clicked)
+btn.grid(column=1, row=0)
+window.mainloop()
